b4284bf4f2f2043811b798f70da489312c749362
gk-yadav0 <gky144546@gmail.com>
2026-09-10 13:50:52 +0530

    Organize basics programs

---
+++ 01_basics/add_two_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int c ;
+  int d ;
+  c=27;
+  d=3;
+  cout<<"enter number" <<c+d<<endl;
+  return 0;
+  }             
---
+++ 01_basics/arithmetic_operations.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a ;
+  int b ;
+  a=36;
+  b=3;
+  cout<<a+b<<endl;
+  cout<<a-b<<endl;
+  cout<<a/b<<endl;
+  cout<<a*b;
+  return 0;
+  }
